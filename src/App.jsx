6d0c91103d22920b1f53740ea3e04512418ff9e1
Miguel <mikeyaceves2003@gmail.com>
2026-02-02 10:52:42 -0800

    Updating App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,53 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode for details.</p>
+        </section>
+      );
+    }
+    return (
+      <section className="details">
+        <h1>Episode {selectedEpisode.id}</h1>
+        <h2>{selectedEpisode.title}</h2>
+        <p>{selectedEpisode.description}</p>
+        <button>Watch Now</button>
+      </section>
+    );
+  }
+
+  function Selection() {
+    return (
+      <section className="episodes">
+        <h2>Episodes</h2>
+        <ul className="episodes">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main>
+        <Selection />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
